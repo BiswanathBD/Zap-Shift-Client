016807c0f6b6b6d3005e77de1d96f2a93027fbe5
BiswanathBD <biswanath.sarker.bd@gmail.com>
2025-11-17 13:44:38 +0600

    complete how it works section

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../Components/Banner";
+import HowItWorks from "../Components/HowItWorks";
 
 const HomePage = () => {
   return (
     <div>
       <Banner />
+      <HowItWorks/>
     </div>
   );
 };
